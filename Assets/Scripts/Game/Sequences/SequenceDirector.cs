23b6b92ca5c7d121c2cea435f14de0bb38ef2d01
tungnguyen2046 <tung.nguyen2046@gmail.com>
2026-05-04 14:24:01 +0700

    fix bug director

--- Assets/Scripts/Game/Sequences/SequenceDirector.cs
+++ Assets/Scripts/Game/Sequences/SequenceDirector.cs
@@ -119,7 +119,7 @@ public class SequenceDirector : MonoBehaviour
             }
         }
 
-        if (step.hasSequenceEvents && eventDirector.simulationFinished) yield return null;
+        if (step.hasSequenceEvents && !eventDirector.simulationFinished) yield return null;
     }
 
     public void PerformAction()
