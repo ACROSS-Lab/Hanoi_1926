859f6976e158e42f78cbede78cf4d2ec4d8d4ae1
elandes <landes.eloi@gmail.com>
2026-05-14 14:28:02 +0700

    Fixing the whole scenario

--- Assets/Scripts/Game/Sequences/SequenceStep.cs
+++ Assets/Scripts/Game/Sequences/SequenceStep.cs
@@ -48,7 +48,9 @@ public class SequenceStep : ScriptableObject
             {"Idle 2", 4},
             {"Looking Down", 5},
             {"Surprised", 6},
-            {"Pointing Right", 7}
+            {"Pointing Right", 7},
+            {"Waving", 8},
+            {"Open Right Hand", 9}
         };
     }
 
